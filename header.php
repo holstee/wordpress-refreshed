d7204647179368cf64db9ea4c52c207e818808eb
Dave <dave@radparvar.com>
2012-11-22 00:44:12 -0500

    trying fixed side

--- header.php
+++ header.php
@@ -58,6 +58,35 @@
 			<?php echo of_get_option('of_theme_css'); ?>
 		<?php } ?>
 	</style>
+
+	<!-- javascript for fixedside -->
+	$(function() {
+    // Stick the #fixedside to the top of the window
+    var nav = $('#fixedside');
+    var navHomeY = nav.offset().top;
+    var isFixed = false;
+    var $w = $(window);
+    $w.scroll(function() {
+        var scrollTop = $w.scrollTop();
+        var shouldBeFixed = scrollTop > navHomeY;
+        if (shouldBeFixed && !isFixed) {
+            nav.css({
+                position: 'fixed',
+                top: 0,
+                left: nav.offset().left,
+                width: nav.width()
+            });
+            isFixed = true;
+        }
+        else if (!shouldBeFixed && isFixed)
+        {
+            nav.css({
+                position: 'static'
+            });
+            isFixed = false;
+        }
+    });
+});
 </head>
 
 <body <?php body_class(); ?>>
