c8d2c8adbf6ec491a71846139b3d7df9db8813ce
Dave <dave@radparvar.com>
2012-11-27 16:16:28 -0500

    udpated name and location sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,20 +1,10 @@
-				<div id="sidebar">			
+				
+					<div id="sidebar">	
+						<!-- wrapper close --><div id="commentWrapper"><!-- wrapper close -->	
 					<!-- grab sidebar widgets -->				
 					<?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('Sidebar') ) : else : ?>		
 					<?php endif; ?>	
 
-<!-- TEST -->	
-					<div id="commentWrapper">
-      <div id="comment">
-        <form>
-        <p class="formrow"><label for="yourname">Name:</label>
-          <input type="text" class="text" id="yourname" name="name" value=""></p>
-        <p class="formrow"><label for="yoururl">URL:</label>
-          <input type="text" class="text" id="yoururl" name="url"></p>
-        <p class="formrow"><textarea rows="10" cols="35" name="body"></textarea></p>
-        <p><input type="button" value="Preview comment"></p>
-        </form>
-      </div>
-    </div>
-<!-- END TEST -->	
+<!-- wrapper close --></div><!-- wrapper close -->
+	
 				</div><!--sidebar-->
