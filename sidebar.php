74643158f91980ff0b029657bf160d7cf72e7d8b
Dave <dave@radparvar.com>
2012-11-28 14:35:27 -0500

    coded in sidebar lightened font

--- sidebar.php
+++ sidebar.php
@@ -5,6 +5,47 @@
 					<?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('Sidebar') ) : else : ?>		
 					<?php endif; ?>	
 
+					<!--sidebar social widgets -->
+
+					<div id="sidebarfixed">
+<span class="socialbox widget">
+<ul>
+	<li>
+		<a href="http://eepurl.com/r5JW5" title="The Weekly Refresh" target="_blank"> 
+			<img src="http://refreshed.is/wp-content/themes/author-child/images/newsletter-icon.png" title="The Weekly Refresh" />
+			<p>The Weekly Refresh  </a><span class="soarrow">&rarr;</span</p>
+	
+	</li>
+	<li>
+		<a href="http://facebook.com/holstee" title="Friend us on Facebook" target="_blank"> 
+			<img src="http://refreshed.is/wp-content/themes/author-child/images/facebook-icon.png" title="Friend us on Facebook" />
+			<p>Friend us on Facebook </a><span class="soarrow">&rarr;</span</p>
+		
+	</li>
+	<li>
+		<a href="http://twitter.com/holstee" title="Follow us on Twitter" target="_blank"> 
+			<img src="http://refreshed.is/wp-content/themes/author-child/images/twitter-icon.png" title="Follow us on Twitter" />
+			<p>Follow us on Twitter  </a><span class="soarrow">&rarr;</span</p>
+		
+	</li>
+	<li>
+		<a href="http://tumblr.refreshed.is" title="Join us on Tumblr" target="_blank"> 
+			<img src="http://refreshed.is/wp-content/themes/author-child/images/tumblr-icon.png" title="Join us on Tumblr" />
+			<p>Join us on Tumblr  </a><span class="soarrow">&rarr;</span</p>
+		
+	</li>
+	<li>
+		<a href="http://feeds.feedburner.com/refreshedis" title="Subscribe to our Feed" target="_blank"> 
+			<img src="http://refreshed.is/wp-content/themes/author-child/images/rss-icon.png" title="Subscribe to our Feed" />
+			<p>Subscribe to our Feed  </a><span class="soarrow">&rarr;</span</p>
+		
+	</li>
+</ul>
+</span>
+</div>
+
+<!-- end sidebar social widgets -->
+
 <!-- wrapper close --></div><!-- wrapper close -->
 	
 				</div><!--sidebar-->
